8f464dcf9f9e2c73be1ff7943fa1bf550f16ec26
Tazim381 <jucse28.381@gmail.com>
2024-01-07 19:16:08 +0600

    update employee design changed

--- client/src/pages/UpdateEmployee.jsx
+++ client/src/pages/UpdateEmployee.jsx
@@ -32,7 +32,7 @@ const UpdateEmployee = () => {
 
 
   useEffect(() => {
-    axios.get(`https://tazim-1234.vercel.app/api/employee/${id}`,{
+    axios.get(`https://tazim-1234.vercel.app/api/employee/${id}`, {
       method: 'GET',
       headers: {
         authorization: `Bearer ${localStorage.getItem(`set-token-for-user`)} `
@@ -59,7 +59,7 @@ const UpdateEmployee = () => {
   const handleSubmit = (event) => {
     event.preventDefault();
     axios
-      .put(`https://tazim-1234.vercel.app/api/employee/update/${id}`, employeeData,{
+      .put(`https://tazim-1234.vercel.app/api/employee/update/${id}`, employeeData, {
         method: 'PUT',
         headers: {
           authorization: `Bearer ${localStorage.getItem(`set-token-for-user`)} `
@@ -76,75 +76,125 @@ const UpdateEmployee = () => {
   };
 
   return (
-    <div className=" bg-slate-400  overflow-hidden flex items-center justify-center">
-      <div className="bg-white lg:w-6/12 md:7/12  rounded-xl mt-10 mb-10">
-        <form className="p-8 " onSubmit={handleSubmit}>
-          <div className='flex justify-center content-center text-3xl font-bold mb-6 md:mb-8 text-gray-800'>Update Employee</div>
-          <div className=" items-center text-lg mb-6 md:mb-8">
-            <p className='font-bold'>Fist Name</p>
-            <input type="text" name="firstName" value={employeeData.firstName} onChange={handleInputChange} className="bg-gray-200 rounded pl-12 py-2 md:py-4 focus:outline-none w-full" placeholder="First Name" required />
-          </div>
-          <div className=" items-center text-lg mb-6 md:mb-8">
-            <p className='font-bold'>Last Name</p>
-            <input type="text" name="lastName" value={employeeData.lastName} onChange={handleInputChange} className="bg-gray-200 rounded pl-12 py-2 md:py-4 focus:outline-none w-full" placeholder="Last Name" required />
-          </div>
-          <div className=" items-center text-lg mb-6 md:mb-8">
-            <p className='font-bold'>Age</p>
-            <input type="number" name="age" value={employeeData.age} onChange={handleInputChange} className="bg-gray-200 rounded pl-12 py-2 md:py-4 focus:outline-none w-full" placeholder="Age" required />
-          </div>
-          <div className=" items-center text-lg mb-6 md:mb-8">
-            <p className='font-bold'>Position</p>
-            <input type="text" name="position" value={employeeData.position} onChange={handleInputChange} className="bg-gray-200 rounded pl-12 py-2 md:py-4 focus:outline-none w-full" placeholder="Position" required />
-          </div>
-          <div className=" items-center text-lg mb-6 md:mb-8">
-            <p className='font-bold'>Email</p>
-            <input type="text" name="email" value={employeeData.email} onChange={handleInputChange} className="bg-gray-200 rounded pl-12 py-2 md:py-4 focus:outline-none w-full" placeholder="Email" required />
-          </div>
-          <div className=" items-center text-lg mb-6 md:mb-8">
-            <p className='font-bold'>Phone</p>
-            <input type="text" name="phone" value={employeeData.phone} onChange={handleInputChange} className="bg-gray-200 rounded pl-12 py-2 md:py-4 focus:outline-none w-full" placeholder="Phone Number" required />
-          </div>
-          <div className=" items-center text-lg mb-6 md:mb-8">
-            <p className='font-bold'>Address</p>
-            <input type="text" name="address" value={employeeData.address} onChange={handleInputChange} className="bg-gray-200 rounded pl-12 py-2 md:py-4 focus:outline-none w-full" placeholder="Address" required />
-          </div>
-          <div className="items-center text-lg mb-6 md:mb-8">
-            <p className='font-bold'>Image URL</p>
-            <input type="text" name="image" value={employeeData.image} onChange={handleInputChange} className="bg-gray-200 rounded pl-12 py-2 md:py-4 focus:outline-none w-full" placeholder="Image URL"  />
-          </div>
-          <div className=" items-center text-lg mb-6 md:mb-8">
-            <p className='font-bold'>Department</p>
-            <input type="text" name="department" value={employeeData.department} onChange={handleInputChange} className="bg-gray-200 rounded pl-12 py-2 md:py-4 focus:outline-none w-full" placeholder="Department" required />
-          </div>
-          <div className=" items-center text-lg mb-6 md:mb-8">
-            <p className='font-bold'>Joining Date</p>
-            <input type="text" name="joiningDate" value={employeeData.joiningDate} onChange={handleInputChange} className="bg-gray-200 rounded pl-12 py-2 md:py-4 focus:outline-none w-full" placeholder="Joining Date" required />
-          </div>
-          <div className=" items-center text-lg mb-6 md:mb-8">
-            <p className='font-bold'>Salary</p>
-            <input type="number" name="salary" value={employeeData.salary} onChange={handleInputChange} className="bg-gray-200 rounded pl-12 py-2 md:py-4 focus:outline-none w-full" placeholder="Salary" required />
-          </div>
-          <div className=" items-center text-lg mb-6 md:mb-8">
-            <p className='font-bold'>Skills</p>
-            <input type="text" name="skills" value={employeeData.skills} onChange={handleInputChange} className="bg-gray-200 rounded pl-12 py-2 md:py-4 focus:outline-none w-full" placeholder="Skills" required />
-          </div>
+    <form className='lg:flex justify-center mt-10 mb-10 ' onSubmit={handleSubmit} >
+      <div class="border border-yellow-700 p-10 bg-slate-100 rounded-lg">
+        <div class="border-b border-gray-900/10 pb-12">
+          <h2 class="text-base font-semibold leading-7 text-gray-900 ">Update Employee Information</h2>
+          <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-9">
+            <div class="sm:col-span-4">
+              <label class="block text-sm font-medium leading-6 text-gray-900">First name</label>
+              <div class="mt-2">
+                <input type="text" name="firstName" value={employeeData.firstName} onChange={handleInputChange} class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" required />
+              </div>
+            </div>
 
-          <div className=" items-center text-lg mb-6 md:mb-8">
-            <p className='font-bold'>Degree</p>
-            <input type="text" name="degree" onChange={handleInputChange} value={employeeData.education && employeeData.education.length > 0 ? employeeData.education[0].degree : ""} className="bg-gray-200 rounded pl-12 py-2 md:py-4 focus:outline-none w-full" placeholder="Degree" required />
+            <div class="sm:col-span-4">
+              <label class="block text-sm font-medium leading-6 text-gray-900">Last name</label>
+              <div class="mt-2">
+                <input type="text" name="lastName"  value={employeeData.lastName} onChange={handleInputChange} class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" required />
+              </div>
+            </div>
+
+            <div class="sm:col-span-4">
+              <label class="block text-sm font-medium leading-6 text-gray-900">Email address</label>
+              <div class="mt-2">
+                <input name="email" type="email" value={employeeData.email} onChange={handleInputChange} class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" required />
+              </div>
+            </div>
+
+            <div class="sm:col-span-3">
+              <label class="block text-sm font-medium leading-6 text-gray-900">Age</label>
+              <div class="mt-2">
+                <input name="age" type="number" value={employeeData.age} onChange={handleInputChange} class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" required />
+              </div>
+            </div>
+
+            <div class="sm:col-span-3">
+              <label class="block text-sm font-medium leading-6 text-gray-900">Phone Number</label>
+              <div class="mt-2">
+                <input type="number" name="phone" value={employeeData.phone} onChange={handleInputChange} class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" required />
+              </div>
+            </div>
+
+
+            <div class="sm:col-span-3">
+              <label class="block text-sm font-medium leading-6 text-gray-900">Address</label>
+              <div class="mt-2">
+                <input type="text" name="address" value={employeeData.address} onChange={handleInputChange} class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" required />
+              </div>
+            </div>
+
+
+            <div class="sm:col-span-3">
+              <label class="block text-sm font-medium leading-6 text-gray-900">Position</label>
+              <div class="mt-2">
+                <input type="text" name="position"  value={employeeData.position} onChange={handleInputChange}class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" required />
+              </div>
+            </div>
+
+            <div class="sm:col-span-3">
+              <label class="block text-sm font-medium leading-6 text-gray-900">Department</label>
+              <div class="mt-2">
+                <input type="text" name="department" value={employeeData.department} onChange={handleInputChange} class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" required />
+              </div>
+            </div>
+
+            <div class="sm:col-span-2">
+              <label class="block text-sm font-medium leading-6 text-gray-900">Salary</label>
+              <div class="mt-2">
+                <input type="number" name="salary" value={employeeData.salary} onChange={handleInputChange} class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" required />
+              </div>
+            </div>
+
+            <div class="sm:col-span-3">
+              <label class="block text-sm font-medium leading-6 text-gray-900">Joining Date</label>
+              <div class="mt-2">
+                <input type="text" name="joiningDate" value={employeeData.joiningDate} onChange={handleInputChange} class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" required />
+              </div>
+            </div>
+
+            <div class="sm:col-span-3 sm:col-start-1">
+              <label class="block text-sm font-medium leading-6 text-gray-900">Degree</label>
+              <div class="mt-2">
+                <input type="text" name="degree"  value={employeeData.education && employeeData.education.length > 0 ? employeeData.education[0].degree : ""} onChange={handleInputChange} class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" required />
+              </div>
+            </div>
+
+            <div class="sm:col-span-3">
+              <label class="block text-sm font-medium leading-6 text-gray-900">University</label>
+              <div class="mt-2">
+                <input type="text" name="university"  value={employeeData.education && employeeData.education.length > 0 ? employeeData.education[0].university : ""} onChange={handleInputChange} class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" required />
+              </div>
+            </div>
+
+            <div class="sm:col-span-2">
+              <label class="block text-sm font-medium leading-6 text-gray-900">Graduation Year</label>
+              <div class="mt-2">
+                <input type="number" name="graduationYear"  value={employeeData.education && employeeData.education.length > 0 ? employeeData.education[0].graduationYear : ""} onChange={handleInputChange} class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" required />
+              </div>
+            </div>
+
+            <div class="sm:col-span-4">
+              <label class="block text-sm font-medium leading-6 text-gray-900">Skills</label>
+              <div class="mt-2">
+                <input type="text" name="skills" value={employeeData.skills} onChange={handleInputChange} class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" required />
+              </div>
+            </div>
+
+            <div class="sm:col-span-3">
+              <label class="block text-sm font-medium leading-6 text-gray-900">Image Url</label>
+              <div class="mt-2">
+                <input type="text" name="image" value={employeeData.image} onChange={handleInputChange} class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+              </div>
+            </div>
+
           </div>
-          <div className=" items-center text-lg mb-6 md:mb-8">
-            <p className='font-bold'>University</p>
-            <input type="text" name="university" onChange={handleInputChange} value={employeeData.education && employeeData.education.length > 0 ? employeeData.education[0].university : ""} className="bg-gray-200 rounded pl-12 py-2 md:py-4 focus:outline-none w-full" placeholder="University" required />
-          </div>
-          <div className=" items-center text-lg mb-6 md:mb-8">
-            <p className='font-bold'>Graduation Year</p>
-            <input type="number" name="graduationYear" onChange={handleInputChange} value={employeeData.education && employeeData.education.length > 0 ? employeeData.education[0].graduationYear : ""} className="bg-gray-200 rounded pl-12 py-2 md:py-4 focus:outline-none w-full" placeholder="Graduation Year" required />
-          </div>
-          <button type="submit" className="border-gray-50 bg-gradient-to-b from-gray-700 to-gray-900 font-medium p-2 md:p-4 text-white uppercase w-full rounded">Update</button>
-        </form>
+        </div>
+
+        <button type="submit" class="flex w-full justify-center rounded-md bg-orange-400 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Update</button>
+
       </div>
-    </div>
+    </form>
   )
 }
 
